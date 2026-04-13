2cb421857f686461071a4c1fddae6e58ac64fbd7
AlexShe <8qvcgx8gxb@privaterelay.appleid.com>
2026-04-13 14:26:29 +0500

    Initial Commit

---
+++ LR-09_Shestakov_AY_ET-112/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LR-09_Shestakov_AY_ET-112
+//
+//  Created by Alexander on 4/13/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
